af47488fd93f848dc28336e2a99e9a52ee11d1ee
Dilnavoz <dilnavozkhonodilova@gmail.com>
2025-11-19 15:32:58 +0500

    site part 1

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <div
+      style={{
+        boxSizing: "border-box",
+        backgroundColor: "#0D192C",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: 0,
+        fontFamily: "Outfit, sans-serif",
+        color: "#FFFFFF",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#15263F",
+          width: "350px",
+          padding: "24px",
+          borderRadius: "15px",
+        }}
+        className="card"
+      >
+        <img
+          className="nft-image"
+          src="/Rectangle Copy.svg"
+          alt="Rasm bor"
+          style={{ width: "100%", borderRadius: "8px", marginBottom: "24px" }}
+        />
+
+        <h2 style={{ fontSize: "22px", fontWeight: 600, marginBottom: "16px" }}>
+          Equilibrium #3429
+        </h2>
+        <p
+          style={{
+            fontSize: "18px",
+            fontWeight: 300,
+            color: "#8BACD9",
+            marginBottom: "24px",
+          }}
+        >
+          Our Equilibrium collection promotes balance and calm.
+        </p>
+
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <div style={{ display: "flex" }}>
+            <img
+              src="./public/images/Combined Shape.svg"
+              alt="bu yerda rasm bor"
+              style={{
+                width: "11px",
+                height: "18px",
+                color: "#00FFF8",
+                marginRight: "7px",
+              }}
+            />
+            <p
+              style={{
+                fontSize: "16px",
+                fontWeight: 600,
+                margin: 0,
+                color: "#00FFF8",
+                marginRight: "110px",
+              }}
+            >
+              {" "}
+              0.041 ETH
+            </p>
+          </div>
+
+          <div style={{ display: "flex" }}>
+            <img
+              src="./public/images/Clock.svg"
+              alt="bu yerda rasm bor"
+              style={{ marginRight: "8px" }}
+            />
+            <p style={{ fontWeight: 400, fontSize: "16px", color: "#8BACD9" }}>
+              3 days left
+            </p>
+          </div>
+        </div>
+        <hr className="hrEl" />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <div style={{display:"flex",alignItems:"center"}}>
+      <img src="/Oval.svg" 
+      alt="" 
+      style={{marginRight:"16px"}}
+      />
+      <span style={{color:"#8BACD9",fontWeight:400,fontSize:"16px"}}>
+        Creation of <span style={{color:"#FFFFFF"}}>Jules Wyvern</span>
+      </span>
+    </div>
+  );
+}
+
+export default Footer;
